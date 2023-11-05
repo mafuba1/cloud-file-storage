4c342b27fc56dcb55c9fecf4f05cb935d0193939
mafuba <tulkidg@gmail.com>
2023-11-05 22:20:52 +0300

    feat(tests): add contextLoads test

--- src/test/java/ru/nasrulaev/cloudfilestorage/CloudFileStorageApplicationTests.java
+++ src/test/java/ru/nasrulaev/cloudfilestorage/CloudFileStorageApplicationTests.java
@@ -1,13 +1,24 @@
 package ru.nasrulaev.cloudfilestorage;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.nasrulaev.cloudfilestorage.repositories.PeopleRepository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class CloudFileStorageApplicationTests {
+
+    @Autowired
+    private PeopleRepository peopleRepository;
 
 	@Test
 	void contextLoads() {
+        assertNotNull(peopleRepository);
 	}
 
 }
